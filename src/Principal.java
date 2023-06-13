38cf9b67ccf0a790c74b8cad1823755ca1836744
olive1rax <contato@olive1ra.xyz>
2023-06-13 13:48:15 -0300

    Update

--- src/Principal.java
+++ src/Principal.java
@@ -1,13 +1,15 @@
 import xyz.olive1ra.catalogo.modelos.Filme;
+import xyz.olive1ra.catalogo.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Tron - o Legado";
-        meuFilme.anoDeLancamento = 2006;
-        meuFilme.duracaoEmMinutos = 140;
+        meuFilme.setNome("Tron - o Legado");
+        meuFilme.setAnoDeLancamento(2006);
+        meuFilme.setIncluidoNoPlano(true);
+        meuFilme.setDuracaoEmMinutos(140);
 
-        meuFilme.exibeFicha();
+        meuFilme.exibeFichaTecnica();
 
         meuFilme.avalia(10);
         meuFilme.avalia(5);
@@ -15,5 +17,11 @@ public class Principal {
         System.out.println("Total de Avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println("Nota: " + meuFilme.pegaMedia());
 
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
     }
 }
--- src/xyz/olive1ra/catalogo/modelos/Filme.java
+++ src/xyz/olive1ra/catalogo/modelos/Filme.java
@@ -1,31 +1,13 @@
 package xyz.olive1ra.catalogo.modelos;
 
-public class Filme {
-    public String nome;
-    public int anoDeLancamento;
-    public boolean incluidoNoPlano;
-    private double somaAvaliacoes;
-    private int totalDeAvaliacoes;
-    public int duracaoEmMinutos;
+public class Filme extends Titulo {
+    private String diretor;
 
-    public int getTotalDeAvaliacoes(){
-        return totalDeAvaliacoes;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void exibeFicha(){
-        System.out.println("Nome xyz.olive1ra.catalogo.modelos.Filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-        System.out.println("Duração: " + duracaoEmMinutos + " minutos.");
-        System.out.println("Incluido no plano: " + incluidoNoPlano);
-
-    }
-
-    public void avalia(double nota){
-        somaAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double pegaMedia(){
-        return somaAvaliacoes / totalDeAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
