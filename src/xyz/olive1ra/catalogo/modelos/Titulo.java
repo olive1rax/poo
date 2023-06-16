c24fd1f31a4b23486153a1aaa57f3846d650141c
olive1rax <contato@olive1ra.xyz>
2023-06-16 11:23:05 -0300

    Update

--- src/xyz/olive1ra/catalogo/modelos/Titulo.java
+++ src/xyz/olive1ra/catalogo/modelos/Titulo.java
@@ -8,7 +8,7 @@ public class Titulo {
     private int totalDeAvaliacoes;
     private int duracaoEmMinutos;
 
-    public String getAmor() {
+    public String getNome() {
         return nome;
     }
 
